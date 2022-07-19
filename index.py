d1e92dca6b21025075cb2421c234652d766cc18a
anirudtate <anirud.programming@gmail.com>
2022-07-19 22:04:59 +0530

    homw work

--- index.py
+++ index.py
@@ -2,14 +2,29 @@ from flask import Flask,render_template, request
 
 app = Flask(__name__)
 
-todos = []
-@app.route("/", methods=['GET', 'POST'])
+@app.route('/')
 def index():
+    return render_template("index.html")
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+@app.route('/user/<username>')
+def profile(username):
+   if username=="anirud":
+       return "This is a admin page"
+   else:
+       return f"This is {username}'s page"
+
+todos = []
+@app.route("/todos", methods=['GET', 'POST'])
+def todolist():
     todo = ""
     if request.method == 'POST':
         todo = request.form['todo']
         todos.insert(0,todo)
-    return render_template("index.html", todos=todos)
+    return render_template("todo.html", todos=todos)
 
 if __name__ == "__main__":
     app.run(debug=True)
